b125b827f57aca455e9bab7bc6bc3963d288c3d2
Filip Tatarnik <ftatarnik@gmail.com>
2020-07-23 09:43:53 -0500

    continueing on CSS

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,41 +10,44 @@ import {
   Button,
 } from "react-bootstrap"
 import { Link } from "gatsby"
+import "./styles.css"
 
 export default class navbar extends Component {
   render() {
     return (
       <div>
         <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">All Mirror and Glass</Navbar.Brand>
+          <Navbar.Brand href="#home" className="hvr-shutter-in-horizontal">
+            All Mirror and Glass
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/">Home</Link>
               </Nav.Link>
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/custommirrors/">Custom Mirrors</Link>
               </Nav.Link>
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/showerdoors/">Shower Doors</Link>
               </Nav.Link>
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/bathaccessories/">Bath Accessories</Link>
               </Nav.Link>
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/commercialstorefront/">Commercial Storefront</Link>
               </Nav.Link>
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/ourservices/">Our Services</Link>
               </Nav.Link>
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/metalfinishes/">Metal Finishes</Link>
               </Nav.Link>
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/glasssamples/">Glass Samples</Link>
               </Nav.Link>
-              <Nav.Link>
+              <Nav.Link className="hvr-float-shadow">
                 <Link to="/links/">Links</Link>
               </Nav.Link>
             </Nav>
